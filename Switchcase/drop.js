f65b57bc6f41384162c25d0d8f1e9160c516f495
khushali3110 <jikadrakhushali@gmail.com>
2025-05-17 14:48:18 +0530

    push

---
+++ Switchcase/drop.js
@@ -0,0 +1,36 @@
+function calc() {
+    // alert(drop)
+    let value1 = document.getElementById("value1").value;
+    let value2 = document.getElementById("value2").value;
+    let drop = document.getElementById("drop").value;
+    let output;
+    let op;
+
+    switch (drop) {
+        case "1":
+            output = Number(value1) + Number(value2);
+            op = "addition";
+            break;
+        case "2":
+            output = Number(value1) - Number(value2);
+            op = "subtraction";
+            break;
+        case "3":
+            output = Number(value1) * Number(value2);
+            op = "multipliaction";
+            break;
+        case "4":
+            output = (Number(value1) / Number(value2)).toFixed(2);
+            op = "division";
+            break;
+        case "5":
+            output = Number(value1) % Number(value2);
+            op = "remender";
+            break;
+        case 6:
+            alert("wrong choice");
+            break;
+    }
+    const result = `${op} ${value1} & ${value2} = ${output}`;
+    document.getElementById("ack").innerHTML = result
+}
